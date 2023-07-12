a60ee4aced14d97081b39fb785f277a51ef7bbc4
unknown <annieyashley@gmail.com>
2023-07-12 12:22:16 -0500

    working links, adds header framework

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
+import Header from "./components/Header"
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       
+      <Header/>
+
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/login" element={<Login />} />
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,32 @@ import { UserContext } from "../ctx/UserContext"
 import {createContext, useContext, useState, useEffect} from "react"
 
 const Login = () => {
+  // const {currUser, logout} = UserContext()
+  const defForm = { email: "", password: "" }
+  const [ formData, setFormData ] = useState(defForm)
+  const [ loginResult, setLoginResult ] = useState("")
+
+  const handleInputChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault()
+    const query = await fetch("/api/user/auth", {
+      method: "post",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const result = await query.json()
+
+    if( result && result.payload ){
+      window.location.href = "/"
+    } else {
+      setLoginResult("fail")
+    }
+  }
 
   return (
     <div>
